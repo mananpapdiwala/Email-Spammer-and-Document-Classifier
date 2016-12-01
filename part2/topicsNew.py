e5265d0f0886693e57240e48b92a86f6b3a9d7ca
Pratik <jainps@iu.edu>
2016-12-01 16:56:31 -0500

    print and small changes

--- part2/topicsNew.py
+++ part2/topicsNew.py
@@ -1,13 +1,10 @@
 # Usage:
 #   ./topics mode dataset-directory model-file [fraction]
-import json
-import os
-import sys
-import math
-
-import re
-
-import time
+from json import load, dump
+from os import listdir
+from sys import exit, argv
+from math import log10
+from re import sub
 from random import randint, choice
 
 # Words to be ignored while learning
@@ -64,15 +61,54 @@ STOPWORDS = {'all', 'pointing', 'whoever', 'four', 'go', 'mill', 'oldest', 'seem
              'sides', 'began', 'younger', 'e', 'longer', 'came', 'backed', 'together', 'u', 'presenting', 'serious'}
 
 
+def calc_p_T(dc):
+    dc.p_T = {}
+    for topic in dc.docCountInTopic:
+        dc.p_T[topic] = dc.docCountInTopic[topic] * 1.0 / dc.totalDocCount
+    return dc
+
+
+def calc_p_word_in_topic(dc):
+    dc.p_word_in_topic = {}
+    for word in dc.p_w_t.keys():
+        dc.p_word_in_topic[word] = {}
+        for topic in dc.p_w_t[word].keys():
+            dc.p_word_in_topic[word][topic] = (dc.p_w_t[word][topic] * 1.0) / dc.words_in_topic[topic]
+    return dc
+
+
+def write_to_file(file_name, writeData):
+    with open(file_name, "w+") as f:
+        dump(writeData, f, indent=2)
+
+
+def print_matrix(topics, confusion_matrix):
+    print '            ',
+    for t in topics:
+        print '%-12s' % t,
+    print ''
+    for actual_topic in topics:
+        print '%-12s' % actual_topic,
+        for test_topic in topics:
+            if actual_topic in confusion_matrix:
+                if test_topic in confusion_matrix[actual_topic]:
+                    print '%-12i' % confusion_matrix[actual_topic][test_topic],
+                else:
+                    print '%-12i' % 0,
+            else:
+                for i in range(20):
+                    print '%-12i' % 0,
+        print ''
+
+
 def testData(words, probabilityTable, p_T):
     p = {}
     for topic in p_T:
         p[topic] = 0
-        words = [word for word in words if word not in STOPWORDS]
         for word in words:
             if word in probabilityTable:
                 if topic in probabilityTable[word]:
-                    p[topic] += math.log10(probabilityTable[word][topic])
+                    p[topic] += log10(probabilityTable[word][topic])
                 else:
                     p[topic] += -6
             else:
@@ -84,7 +120,7 @@ def testData(words, probabilityTable, p_T):
 
 
 def get_dir_contents(directory):
-    topics = os.listdir(directory)  # list of all topics
+    topics = listdir(directory)  # list of all topics
     for word in topics[:]:
         if word.startswith('.'):
             topics.remove(word)
@@ -92,7 +128,43 @@ def get_dir_contents(directory):
 
 
 def get_words(content):
-    return re.sub('[^a-zA-Z \n]', '', content).lower().split()
+    return [word for word in sub('[^a-zA-Z \n]', '', content).lower().split() if word not in STOPWORDS]
+
+
+def validate_initialize():
+    fraction = 0  # initialize fraction to 0
+    if len(argv) > 5:  # check if too many arguments
+        print("Too many arguments!")
+        exit(1)
+    if len(argv) < 4:  # check if too few arguments
+        print("Too few arguments!")
+        exit(1)
+    mode = argv[1]
+    dataset_directory = argv[2]
+    model_file = argv[3]
+    if mode == "train":
+        if len(argv) == 5:
+            try:
+                fraction = float(argv[4])
+                if fraction < 0 or fraction > 1:  # check invalid fraction
+                    print("Fraction should be a number between 0.0 and 1.0")
+                    exit(1)
+            except:  # invalid fraction
+                print("Fraction should be a number between 0.0 and 1.0")
+                exit(1)
+        else:  # invalid input
+            print("Train mode takes 4 command line arguments")
+            exit(1)
+    elif mode == "test":
+        if len(argv) != 4:
+            print("Test mode takes 3 command line arguments")
+            exit(1)
+    else:  # invalid mode
+        print("Invalid mode!")
+        exit(1)
+    bias = fraction * 100
+    topics = get_dir_contents(dataset_directory)
+    return bias, mode, dataset_directory, model_file, topics
 
 
 class DocumentClassification:
@@ -100,7 +172,6 @@ class DocumentClassification:
         self.docCountInTopic = {}
         self.words_in_topic = {}
         self.p_w_t = {}
-        self.count_wordDoc_per_topic = {}
         self.totalDocCount = 0
         pass
 
@@ -125,42 +196,10 @@ class DocumentClassification:
 
 
 if __name__ == "__main__":
-    fraction = 0  # initialize fraction to 0
-    if len(sys.argv) > 5:  # check if too many arguments
-        print("Too many arguments!")
-        sys.exit(1)
-    if len(sys.argv) < 4:  # check if too few arguments
-        print("Too few arguments!")
-        sys.exit(1)
-    mode = sys.argv[1]
-    dataset_directory = sys.argv[2]
-    model_file = sys.argv[3]
-    if mode == "train":
-        if len(sys.argv) == 5:
-            try:
-                fraction = float(sys.argv[4])
-                if fraction < 0 or fraction > 1:  # check invalid fraction
-                    print("Fraction should be a number between 0.0 and 1.0")
-                    sys.exit(1)
-            except:  # invalid fraction
-                print("Fraction should be a number between 0.0 and 1.0")
-                sys.exit(1)
-        else:  # invalid input
-            print("Train mode takes 4 command line arguments")
-            sys.exit(1)
-    elif mode == "test":
-        if len(sys.argv) != 4:
-            print("Test mode takes 3 command line arguments")
-            sys.exit(1)
-    else:  # invalid mode
-        print("Invalid mode!")
-        sys.exit(1)
-
+    bias, mode, dataset_directory, model_file, topics = validate_initialize()
     if mode == 'train':
-        start_time = time.time()
+        print "Training...\nIt may take a few minutes."
         dc = DocumentClassification()
-        bias = fraction * 100
-        topics = get_dir_contents(dataset_directory)
         unknownList = dict()
         for topic in topics:
             documents = get_dir_contents(dataset_directory + "/" + topic)
@@ -170,65 +209,53 @@ if __name__ == "__main__":
                     content = f.read()
                 # flip a coin according to fraction
                 flip = randint(0, 100)
-                if flip > bias:  # topic  = unknown if flip > bias
+                if flip > bias or (flip == 0 and bias == 0):  # topic  = unknown if flip > bias
                     unknownList[dataset_directory + "/" + topic + "/" + document] = get_words(content)
                     continue
-                words = [word for word in get_words(content) if word not in STOPWORDS]
+                words = get_words(content)
                 dc.processDocument(words, topic)
-
-        dc.p_word_in_topic = {}
-        for word in dc.p_w_t.keys():
-            dc.p_word_in_topic[word] = {}
-            for topic in dc.p_w_t[word].keys():
-                dc.p_word_in_topic[word][topic] = (dc.p_w_t[word][topic] * 1.0) / dc.words_in_topic[topic]
-
-        dc.p_T = {}
-        for topic in dc.docCountInTopic:
-            dc.p_T[topic] = dc.docCountInTopic[topic] * 1.0 / dc.totalDocCount
-
+        dc = calc_p_word_in_topic(dc)
+        dc = calc_p_T(dc)
         if len(unknownList) != 0:
-            for unknown_file in unknownList:
-                label = testData(unknownList[unknown_file], dc.p_word_in_topic, dc.p_T)
-                dc.processDocument(unknownList[unknown_file], label)
-
-            dc.p_word_in_topic = {}
-            for word in dc.p_w_t.keys():
-                dc.p_word_in_topic[word] = {}
-                for topic in dc.p_w_t[word].keys():
-                    dc.p_word_in_topic[word][topic] = (dc.p_w_t[word][topic] * 1.0) / dc.words_in_topic[topic]
-
-            dc.p_T = {}
-            for topic in dc.docCountInTopic:
-                dc.p_T[topic] = dc.docCountInTopic[topic] * 1.0 / dc.totalDocCount
+            if bias == 0:
+                for i in range(10):
+                    dc.__init__()
+                    for unknown_file in unknownList:
+                        label = testData(unknownList[unknown_file], dc.p_word_in_topic, dc.p_T)
+                        dc.processDocument(unknownList[unknown_file], label)
+                    dc = calc_p_word_in_topic(dc)
+                    dc = calc_p_T(dc)
+            else:
+                for unknown_file in unknownList:
+                    label = testData(unknownList[unknown_file], dc.p_word_in_topic, dc.p_T)
+                    dc.processDocument(unknownList[unknown_file], label)
+                dc = calc_p_word_in_topic(dc)
+                dc = calc_p_T(dc)
         # Write model to file
         writeData = {
             'p_word_in_topic': dc.p_word_in_topic,
             'p_T': dc.p_T,
         }
-        with open(model_file, "w+") as f:
-            json.dump(writeData, f, indent=2)
+        write_to_file(model_file, writeData)
         top_words_per_topic = {}
         for topic in topics:
             top_words_per_topic[topic] = sorted(
-                [[dc.p_w_t[word][t], word] for word in dc.p_w_t for t in dc.p_w_t[word] if
-                 word not in STOPWORDS and t == topic], key=lambda element: element[0], reverse=True)[:10]
-        with open("distinctive_words.txt", "w+") as f:
-            json.dump(top_words_per_topic, f, indent=4)
-        print time.time() - start_time
+                [[dc.p_w_t[word][t], word] for word in dc.p_w_t for t in dc.p_w_t[word] if t == topic],
+                key=lambda element: element[0], reverse=True)[:10]
+        write_to_file("distinctive_words.txt", top_words_per_topic)
     else:  # test mode
+        print "Testing...\nIt may take a few minutes."
         # read the learned model
         count_doc = 0
-        count_accuracy_p3 = 0
+        accuracy = 0
         try:
             with open(model_file) as f:
-                readData = json.load(f)
+                readData = load(f)
             p_T = readData['p_T']
             p_word_in_topic = readData['p_word_in_topic']
-            print p_T
         except:
             print "Invalid Model File!"
-            sys.exit(1)
-        topics = get_dir_contents(dataset_directory)
+            exit(1)
         confusion_matrix = {}
         for topic in topics:
             confusion_matrix[topic] = {}
@@ -239,11 +266,11 @@ if __name__ == "__main__":
                     content = f.read()
                 p3 = testData(get_words(content), p_word_in_topic, p_T)
                 if str(p3) in confusion_matrix[topic]:
-                	confusion_matrix[topic][str(p3)] += 1
+                    confusion_matrix[topic][str(p3)] += 1
                 else:
-                	confusion_matrix[topic][str(p3)] = 1
+                    confusion_matrix[topic][str(p3)] = 1
                 count_doc += 1
                 if topic == p3:
-                    count_accuracy_p3 += 1
-        print "Accuracy for p3: ", (count_accuracy_p3 * 1.0 / count_doc) * 100
-        print confusion_matrix
+                    accuracy += 1
+        print "Accuracy: ", (accuracy * 1.0 / count_doc) * 100
+        print_matrix(topics, confusion_matrix)
